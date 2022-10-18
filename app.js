178876ccd6ff57b84ee1ae5835b9e31db6bc50fb
24HourDeveloper <latoya30310@gmail.com>
2022-10-18 19:18:11 -0400

    capitalize folder name

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use('/user', require('./controllers/UserRouter'))
 
 
 app.get('/', (req, res) => {
-    res.render('pages/HomePage')
+    res.render('Pages/HomePage')
 })
 
 app.listen(PORT, () => {
